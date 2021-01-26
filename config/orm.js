cacb315ff6e258d914401c1b1d2479cd0c1402c0
Kyle Chriscoe <kyle717@gmail.com>
2021-01-26 16:01:13 -0500

    added ORM functions to ORM.js

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,94 @@
 // ===Importing MySQL connection dependency===
 const connection = require("../config/connection.js");
 
-const orm = {
+// ===SQL Syntax Helper Functions===
+function printQuestionMarks(num) {
+    // Function to return a string of comma separated "?"s
+    // Input: (Int) Number
+    // Output: (String) of comma separated "?"s matching input num
 
+    let arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
 }
+
+function objToSql(ob) {
+    // Function to convert object key/value pairs to SQL syntax
+    // Input: (Object) with keys and values
+    // Output: (String) single comma separated string
+
+    let arr = [];
+
+    for(var key in ob) {
+        let value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+
+const orm = {
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    create: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    update: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
 
 // ===Export ORM object===
 module.exports = orm;
